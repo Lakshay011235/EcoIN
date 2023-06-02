1105688e69c08b075962213cda64379a33338225
Anirudh3167 <77240291+Anirudh3167@users.noreply.github.com>
2023-06-02 10:46:01 +0530

    some changes made

--- backend/carbonfootprint/urls.py
+++ backend/carbonfootprint/urls.py
@@ -4,5 +4,7 @@ from .views import *
 app_name = "Carbon"
 
 urlpatterns = [
-    path('',Home,name="Home")
+    path('',Home,name="Home"),
+    path('individual-calculator',IndividualCalculator,name="IndividualCalculator"),
+    path('business-calculator',BusinessCalculator,name="BusinessCalculator"),
 ]
--- backend/carbonfootprint/views.py
+++ backend/carbonfootprint/views.py
@@ -1,20 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def Home(request):
-    return render(request,"general/home.html")
+    return render(request,'carbonfootprint/perspective.html')
 
-def Calculator(request):
+def IndividualCalculator(request):
     if request.method == "POST":
+        emission = 0
         current = request.POST['electricity']   # In units = KWH
-        gas = request.POST['LPG']   # In grams
+        gas = request.POST['gas']   # In grams
         vehicle = request.POST['vehicle']   # Car , Bus
+        milage = request.POST['mileage']
+        distance = request.POST['distance']
 
         if (vehicle == "Car"):
             car_fuel = request.POST['fuel']     # Data is shared in the group
         elif (vehicle == "Bus"):
             bus_fuel = request.POST['fuel']     # Data is shared in the group
 
-        # Calculator algo here
+        # Fuel
+
+        # Electricity
         
-    return render(request,'general/home.html')
+        return HttpResponse("<h1> You carbon emission is:"+str(emission)+" </h1>")
+    return render(request,"carbonfootprint/individual.html")
+
+def BusinessCalculator(request):
+    return render(request,"general/home.html")
