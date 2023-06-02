2264cad48ae1e681accbc765cc1239fc78c3ed94
SanchitMahajan236 <mahajansanchit236@gmail.com>
2023-06-02 16:03:35 +0530

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tbv!y=rq^j$y3r_7(1a!6$+h-_%thx0k7$2&t7hab34l%nj&!a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
