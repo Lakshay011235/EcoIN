4b928ec153b092983b78ef5c1544ca1976604493
Anirudh3167 <77240291+Anirudh3167@users.noreply.github.com>
2023-06-02 11:43:34 +0530

    ntg

--- backend/marketplace/views.py
+++ backend/marketplace/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
 from django.forms.models import model_to_dict
+from django.contrib.auth.decorators import login_required
+from .rapyd_apis import make_request
 
 # Create your views here.
 def Home(request):
@@ -24,3 +26,21 @@ def ProductPage(request,id):
     context = product
     print(context)
     return render(request,"marketplace/SingleCard.html",context=context)
+
+@login_required(login_url='Login')
+def PaymentDirection(request):
+    wallet_body = {
+        "amount": 50,
+        "currency": "USD",
+        "country":"US",
+        "complete_payment_url":"Eco_Sotre:Home",
+        #Change this github link at the time of production. to the website link
+        #Rapyd do not allow urls of local host. Thats why we kept github here
+        "complete_checkout_url":"https://www.github.com/anirudh3167",
+        "error_payment_url":"store:Failed"
+    }
+    response = make_request(method='post',path='/v1/checkout',body=wallet_body)
+    #print(response)
+    hosted_url = response['data']['redirect_url']
+    #print("REDIRECT_URL:",hosted_url)
+    return redirect(hosted_url)
