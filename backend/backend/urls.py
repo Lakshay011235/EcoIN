f2eee96c771cdfbc6c2877f10add9c3ac4912262
SanchitMahajan236 <mahajansanchit236@gmail.com>
2023-06-02 17:48:36 +0530

    push

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -26,6 +26,4 @@ urlpatterns = [
     path('store/', include('marketplace.urls', namespace='MarketPlace')),
     path('carbon/', include('carbonfootprint.urls', namespace='Carbon')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
